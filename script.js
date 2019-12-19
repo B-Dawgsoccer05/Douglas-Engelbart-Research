1ea6738a9ec91346f4529cdb6b3922df25d7ba82
B-Dawgsoccer05 <57766682+B-Dawgsoccer05@users.noreply.github.com>
2019-12-19 12:32:21 -0500

    started quiz page

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var question = "What did Douglas Engelbart make?";
 
+var answers = [ 'Computer Mouse', 'The  Computer', 'Term Debug', 'HTML']; 
+
+var answers = document.getElementByClassName("quizButton");
+
+var correct = 1 //The first answer is at spot 1 and the last is at spot 4.
+
+documnet.getElementById("question").innerHTML = question;
+
+for(var spot=0; spot<answers.length; spot++)
+{
+    answerButtons[spot].innerHTML = answers[spot];
+}
