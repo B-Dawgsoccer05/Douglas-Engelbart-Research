9ca0b76e9bc7693ff8aab6733d283ee1c77b404f
B-Dawgsoccer05 <57766682+B-Dawgsoccer05@users.noreply.github.com>
2019-12-20 12:33:39 -0500

    added quiz buttons and pics

--- script.js
+++ script.js
@@ -2,13 +2,29 @@ var question = "What did Douglas Engelbart make?";
 
 var answers = [ 'Computer Mouse', 'The  Computer', 'Term Debug', 'HTML']; 
 
-var answers = document.getElementByClassName("quizButton");
+var answerButtons = document.getElementsByClassName("quizButton");
 
-var correct = 1 //The first answer is at spot 1 and the last is at spot 4.
+var correct = 0; //The first answer is at spot 0 and the last is at spot 3.
 
-documnet.getElementById("question").innerHTML = question;
+document.getElementById("question").innerHTML = question;
 
 for(var spot=0; spot<answers.length; spot++)
 {
     answerButtons[spot].innerHTML = answers[spot];
+
+    answerButtons[spot].addEventListener('click', function() {
+        disableAll();
+        if(this.innerHTML == answers[correct]) {
+            this.setAttribute('style', 'background-color:rgb(76,175,80);');
+        } else {
+            this.setAttribute('style','background-color:rgb(210,50,35);');
+        }
+    });
+
+}
+
+function disableAll() {
+    for(var num=0; num<answerButtons.length; num++) {
+        answerButtons[num].setAttribute('disabled', true);
+    }
 }
